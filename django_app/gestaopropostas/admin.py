d928e1bc14515c58d0c121bb99687a7c84e43bf3
itagabriel <ita32.gabriel@gmail.com>
2023-06-19 22:01:22 -0300

    [main] criacao models.py classe proposta e custom_fields

--- django_app/gestaopropostas/admin.py
+++ django_app/gestaopropostas/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Proposta
+from .models import Proposta, CustomFields
+
+class CustomFieldsInline(admin.TabularInline):
+    model = Proposta.campos_adicionais.through
+    extra = 1
 
 @admin.register(Proposta)
 class PropostaAdmin(admin.ModelAdmin):
-    list_display = ['nome_completo', 'cpf', 'endereco', 'valor_emprestimo', 'status', 'data_proposta']
+    inlines = [CustomFieldsInline]
+    list_display = ('nome_completo', 'cpf', 'endereco', 'valor_emprestimo', 'status', 'data_proposta')
     search_fields = ['nome_completo', 'cpf']
     list_filter = ['status']
+
+@admin.register(CustomFields)
+class CustomFieldsAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tipo_campo')
--- django_app/gestaopropostas/models.py
+++ django_app/gestaopropostas/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 
+class CustomFields(models.Model):
+    TIPO_CAMPO_CHOICES = [
+        ('string', 'Texto'),
+        ('number', 'Numérico'),
+        ('bool', 'Booleano'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    tipo_campo = models.CharField(max_length=6, choices=TIPO_CAMPO_CHOICES)
+    exibir_no_formulario = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        db_table = 'custom_fields'
+        verbose_name_plural = 'Custom Fields'
+
+    
 class Proposta(models.Model):
-    nome_completo = models.CharField(max_length=100)
+    campos_adicionais = models.ManyToManyField(CustomFields)
+    nome_completo = models.CharField(max_length=200)
     cpf = models.CharField(max_length=14)
     endereco = models.CharField(max_length=200)
     valor_emprestimo = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField(default=False)
     data_proposta = models.DateTimeField(auto_now_add=True)
+
+    def get_status_display(self):
+        if self.status:
+            return "Aprovada"
+        else:
+            return "Negada"
 
     def __str__(self):
         return self.nome_completo
